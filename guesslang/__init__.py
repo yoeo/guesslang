3b8801389ccf87aed8c5dc696bef17101dfadf59
Yandaar SOMDA <ysomda@deezer.com>
2018-11-01 11:53:40 +0100

    More precise linter error suppression

--- guesslang/__init__.py
+++ guesslang/__init__.py
@@ -4,9 +4,9 @@ of a given source file.
 
 """
 
-from guesslang.config import config_logging  # noqa
-from guesslang.guesser import Guess  # noqa
-from guesslang.utils import GuesslangError  # noqa
+from guesslang.config import config_logging  # noqa: F401
+from guesslang.guesser import Guess  # noqa: F401
+from guesslang.utils import GuesslangError  # noqa: F401
 
 
 __version__ = '0.9.3'
